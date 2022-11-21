14dcc315b07a57ebbe0e6382957b9e3290285912
okayamayu8 <49719415+okayamayu8@users.noreply.github.com>
2022-11-21 11:53:50 +0900

    Update Falcons.jl

--- src/Falcons.jl
+++ src/Falcons.jl
@@ -13,5 +13,6 @@ include("./function/systematics.jl")
 
 export ScanningStrategy, gen_ScanningStrategy, get_pointings, get_pointing_pixels, period2rpm, get_pointings_tuple, pixtod2hitmap, angtod2hitmap, convert_maps, rpm2angfreq, rpm2period, show_ss
 export get_psiDataBase, get_psi_time_DataBase, ScanningStrategy2MapInfo, set_input, orientation_func, get_hn_map, ScanningStrategy2map
+export npzread
 
 end
